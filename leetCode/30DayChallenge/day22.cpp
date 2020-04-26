c73563436f82a650350b12e65b55704155df5a6f
Lucas <lucasmfromero@gmail.com>
2020-04-26 17:58:40 -0300

    Added leet code challenge day 22

---
+++ leetCode/30DayChallenge/day22.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        map <int, int> m;
+        m[0] = 1;
+        
+        int sum = 0;
+        int counter = 0;
+        
+        for (int x : nums) {
+            sum += x;
+            counter += m[sum-k];
+            m[sum]++;
+        }
+        
+        return counter;
+}
