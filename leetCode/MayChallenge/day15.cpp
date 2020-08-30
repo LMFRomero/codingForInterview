452997d8fbff9d37d0ee1f9f2a048ca8567d09b9
Lucas Romero <48536174+LMFRomero@users.noreply.github.com>
2020-08-30 19:47:33 -0300

    Added leet code challenge May day 10

---
+++ leetCode/MayChallenge/day15.cpp
@@ -0,0 +1,30 @@
+class Solution {
+    
+public:
+    int maxSubarraySumCircular(vector<int>& a) {
+        long long ans = a[0];
+        int first = 0;
+        int n = a.size();
+        long long cur_prefix = 0;
+        vector<pair<int, long long>> s;
+        s.emplace_back(-1, 0LL);
+        
+        for (int i = 0; i < n; i++) {
+            a.push_back(a[i]);
+        }
+        
+        for (int i = 0; i < 2*n; i++) {
+            if (first < s.size() and s[first].first == i-n-1) {
+                first++;
+            }
+            cur_prefix += a[i];
+            if (first < s.size()) ans = max(cur_prefix - s[first].second, ans);
+            while (first < s.size() and s.back().second >= cur_prefix)
+                s.pop_back();
+            
+            s.emplace_back(i, cur_prefix);
+        }
+        
+        return ans;
+    }
+};
